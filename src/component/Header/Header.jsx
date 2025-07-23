ab404cf8c29019b908c135a44087442aa1141474
Ketan Dudka <ketandudka566123@gmail.com>
2025-07-23 11:48:08 +0530

    poush

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import img1 from "../../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import Hamburger from "hamburger-react";
 const Header = () => {
+  const [navBar, setnavBar] = useState(false);
+const location = useLocation()
+  const togglenavs = () => {
+    setnavBar(!navBar);
+  };
+
+  const navlinks = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "#",
+    },
+    {
+      name: "Work",
+      path: "#",
+    },
+    {
+      name: "Services",
+      path: "#",
+    },
+    {
+      name: "Pricing",
+      path: "#",
+    },
+    {
+      name: "Testimonial",
+      path: "#",
+    },
+    {
+      name: "Blogs",
+      path: "/blogs",
+    },
+  ];
+
   return (
     <>
       <div className="header-parent parent bg-img-cover">
@@ -11,15 +49,29 @@ const Header = () => {
             <img src={img1} alt="Website Logo" />
           </div>
           <div className="menu">
-            <Link to="#">Home </Link>
-            <Link to="#">About</Link>
-            <Link to="#">Work</Link>
-            <Link to="#">Services</Link>
-            <Link to="#">Pricing</Link>
-            <Link to="#">Testimonial</Link>
-            <Link to="/blog">Blogs</Link>
+            {navlinks.map((item, index) => (
+              <Link key={index} to={item.path}>
+                {" "}
+                {item.name}{" "}
+              </Link>
+            ))}
           </div>
           <div className="btn">Contact</div>
+
+          <div class="mobile_nav">
+            <div class="hamburger">
+              <Hamburger toggled={navBar} toggle={togglenavs} />
+            </div>
+
+            <div class={navBar ? "nav_list active" : "nav_list"}>
+              {navlinks.map((item, index) => (
+                <Link key={index} to={item.path}  className={location.pathname === item.path ? "active link" : "link"}  >
+                  {" "}
+                  {item.name}{" "}
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
